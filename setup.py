9afa4a20f3b6594e604a2208d4f137a17aad0768
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2024-05-31 15:07:09 +0800

    add requirements.txt and define lib v1.0

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import find_packages, setup
 
+with open("./requirements.txt", "r") as f:
+    requirements = [l.strip() for l in f.readlines() if len(l.strip()) > 0]
+
 setup(
-    name="hm3d-data-collector-dev",
-    version="0.1",
+    name="hm3d-data-collector",
+    version="1.0",
     packages=find_packages(),
+    install_requires=requirements,
     author="Enrique Solarte",
     author_email="enrique.solarte.pardo@gmail.com",
     description=("This is data collector for HM3D dataset"),
